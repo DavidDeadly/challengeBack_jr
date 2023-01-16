51499b1b17db86aa304274a1dc0ffa9df1e81b8c
DavidDeadly <jdrueda513@gmail.com>
2023-01-16 12:29:19 -0500

    initial commit

---
+++ src/main/java/co/sofka/challenge_jr/ChallengeJrApplication.java
@@ -0,0 +1,13 @@
+package co.sofka.challenge_jr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeJrApplication.class, args);
+	}
+
+}
